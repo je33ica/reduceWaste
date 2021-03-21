fe8751c59c99f0de4719f01ff350a5f434e73397
je33ica <je331687@gmail.com>
2021-03-21 16:49:29 +0000

    added bcryptjs for password hashing

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,18 +1,29 @@
 const router = require("express").Router();
-const db = require("../models")
+const db = require("../models");
+const bcrypt = require("bcryptjs");
 
-router.post("/api/sign-up", ({body}, res) => {
+//  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
+//  User.prototype.validPassword = function(password) {
+//     return bcrypt.compareSync(password, this.password);
+//   };
+//   // Hooks are automatic methods that run during various phases of the User Model lifecycle
+//   // In this case, before a User is created, we will automatically hash their password
+//   User.addHook("beforeCreate", function(user) {
+//     user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
+//   });
 
-    const user = new db.User(body);
-    user.lastUpdatedDate();
+router.post("/api/sign-up", ({ body }, res) => {
+  body.password = bcrypt.hashSync(body.password, bcrypt.genSaltSync(10), null);
+  const user = new db.User(body);
+  user.lastUpdatedDate();
 
-    db.User.create(user)
-    .then(newlyCreatedUser =>{
-        res.json(newlyCreatedUser)
+  db.User.create(user)
+    .then((newlyCreatedUser) => {
+      res.json(newlyCreatedUser);
     })
-    .catch(err => {
-        res.json(err)
-    })
-})
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
