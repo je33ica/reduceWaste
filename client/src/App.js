3eebe431ed435b89d93bab792ab396e6b6268877
je33ica <je331687@gmail.com>
2021-04-07 18:23:10 +0100

    back end request test

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
 
+//checking response from back end
 function App() {
+  useEffect(() => {
+    fetch("/api/sign-in", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({ email: "aree@ross.com", password: "password123" }),
+    })
+      .then((res) => res.json())
+      .then((result) => console.log("im the result", result));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p></p>
         <a
           className="App-link"
           href="https://reactjs.org"
