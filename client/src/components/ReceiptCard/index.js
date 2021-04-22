0a210df5b133412b3c0dd5514ee2aeb51ffb53e4
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-22 18:22:49 +0100

    added ean input

--- client/src/components/ReceiptCard/index.js
+++ client/src/components/ReceiptCard/index.js
@@ -1,17 +1,54 @@
-import {card} from "./receiptcard.module.scss"
+import { card } from "./receiptcard.module.scss";
 
-const ReceiptCard = ({product, removeCard, updateElement})=> {
+const ReceiptCard = ({ product, removeCard, updateElement }) => {
   return (
     <div className={card}>
       <label htmlFor="productName">Product</label>
-      <input type="text" name="productName" value={product.productName} onChange={(e) => updateElement(e.target.value, e.target.name, product.id)}/>
-      <label htmlFor="amount">Amount</label>
-      <input type="text" name="amount" value={product.amount} onChange={(e) => updateElement(e.target.value, e.target.name, product.id)}/>
+      <input
+        type="text"
+        name="productName"
+        value={product.productName}
+        onChange={(e) =>
+          updateElement(e.target.value, e.target.name, product.id)
+        }
+        required
+      />
+      <label htmlFor="amount">
+        Amount - <em>Optional</em>
+      </label>
+      <input
+        type="text"
+        name="amount"
+        value={product.amount}
+        onChange={(e) =>
+          updateElement(e.target.value, e.target.name, product.id)
+        }
+      />
+      <label htmlFor="ean">
+        EAN (barcode) - <em>Optional</em>
+      </label>
+      <input
+        type="text"
+        name="ean"
+        value={product.ean}
+        onChange={(e) =>
+          updateElement(e.target.value, e.target.name, product.id)
+        }
+      />
       <label htmlFor="expiry">Expiry</label>
-      <input type="date" name="expiry" value={product.expiry} onChange={(e) => updateElement(e.target.value, e.target.name, product.id)}/>
-      <button type="button" onClick={() => removeCard(product.id)}>Remove Item</button>
+      <input
+        type="date"
+        name="expiry"
+        value={product.expiry}
+        onChange={(e) =>
+          updateElement(e.target.value, e.target.name, product.id)
+        }
+      />
+      <button type="button" onClick={() => removeCard(product.id)}>
+        Remove Item
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default ReceiptCard
+export default ReceiptCard;
