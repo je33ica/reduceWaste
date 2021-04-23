09dc159059e2efad12280b70bf93bd95f38c3204
je33ica <je331687@gmail.com>
2021-04-23 21:05:53 +0100

    start of product from component

--- client/src/components/AddProductForm/index.js
+++ client/src/components/AddProductForm/index.js
@@ -1,23 +1,17 @@
 import { useEffect } from "react";
 
-const AddProductForm = ({
-  productName,
-  productAmount,
-  expiryDate,
-  submitProductHandler,
-}) => {
-  //   submitProductHandler;
+
 
   return (
     <div>
       <form className="form" onSubmit={submitProductHandler}>
         <h2>Add Products</h2>
         <label>Product name</label>
-        <input type="text" name="product" ref={productName} required></input>
+        
         <label>Amount</label>
-        <input type="text" name="amount" ref={productAmount} required></input>
+        
         <label>Expiry</label>
-        <input type="date" name="expiry" ref={expiryDate} required></input>
+       
         <p>
           <button className="btn" type="submit">
             Add product
