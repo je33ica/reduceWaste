77e1c482df1de181d32e2f4453bb5c297fa89e38
je33ica <je331687@gmail.com>
2021-04-23 21:58:34 +0100

    imports updated

--- client/src/pages/AddProducts/index.js
+++ client/src/pages/AddProducts/index.js
@@ -1,11 +1,20 @@
 import { useRef, useState } from "react";
 import AddProductForm from "../../components/AddProductForm";
 import NavBar from "../../components/NavBar";
+import PopUpAlert from "../../components/PopUpAlert";
+import Loading from "../../components/Loading";
 
 const AddProducts = () => {
   const productNameInput = useRef("");
   const productAmountInput = useRef("");
   const expiryDateInput = useRef("");
+
+  const [loading, setLoading] = useState(false);
+  const [displayPopup, setDisplayPopup] = useState({
+    show: false,
+    type: "",
+    message: "",
+  });
 
   const submitProductHandler = (e) => {
     e.preventDefault();
@@ -21,6 +30,14 @@ const AddProducts = () => {
       },
     ];
     console.log("im the new prodcut", addNewProduct);
+
+    //before fetch -> render the loading component
+    //insde 2nd .then -> unrender loading component
+    //render a a pop up alert based on the response
+    //hanlde the promise relut with the succes/fail message - use registration form
+    //the server expects an array
+    setLoading(true);
+    //submit the registration form
     fetch("api/users/products", {
       method: "PUT",
       headers: {
@@ -54,7 +71,6 @@ const AddProducts = () => {
         })
       );
   };
-
   const navBarItems = [
     { path: "/account", text: "Account" },
     { path: "/barcode", text: "Barcode scanner" },
