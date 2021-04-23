2171d806bf831e0520cfa5933c35a7d3a9816c48
je33ica <je331687@gmail.com>
2021-04-23 21:09:18 +0100

    creating inputs for products

--- client/src/components/AddProductForm/index.js
+++ client/src/components/AddProductForm/index.js
@@ -7,11 +7,11 @@ import { useEffect } from "react";
       <form className="form" onSubmit={submitProductHandler}>
         <h2>Add Products</h2>
         <label>Product name</label>
-        
+        <input type="text" name="product" ref={productName} required></input>
         <label>Amount</label>
-        
+        <input type="text" name="amount" ref={productAmount} required></input>
         <label>Expiry</label>
-       
+        <input type="date" name="expiry" ref={expiryDate} required></input>
         <p>
           <button className="btn" type="submit">
             Add product
