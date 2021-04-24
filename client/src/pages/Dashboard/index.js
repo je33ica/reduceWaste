ad6216842e3329e39471d9f9cfdaa5749cdb1fbc
je33ica <je331687@gmail.com>
2021-04-24 10:42:39 +0100

    dashboard page with basic table

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,3 +1,19 @@
-const Dashboard = () => {};
+import DashboardTable from "../../components/Dashboard";
+import NavBar from "../../components/NavBar";
+
+const Dashboard = () => {
+  const navBarItems = [
+    { path: "/account", text: "Account" },
+    { path: "/barcode", text: "Barcode scanner" },
+    { path: "/receipt", text: "Upload receipt" },
+    { path: "/addProducts", text: "Upload receipt" },
+  ];
+  return (
+    <>
+      <NavBar navBarItems={navBarItems} />
+      <DashboardTable />;
+    </>
+  );
+};
 
 export default Dashboard;
