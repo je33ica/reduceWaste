4cd8a5c6cbf2ffe567c476c17c2fec300e862961
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-24 16:39:21 +0100

    added recipe cards that are links and added hide card button

--- client/src/components/RecipeHolder/index.js
+++ client/src/components/RecipeHolder/index.js
@@ -1,19 +1,41 @@
+import { useState } from "react";
 import RecipeCard from "./RecipeCard";
-import { cardContainer } from "./recipe.module.scss";
+import { cardContainer, btn } from "./recipe.module.scss";
 
 const RecipeHolder = ({ searchRecipes, recipes }) => {
+  const [hideRecipes, setHideRecipes] = useState(false);
+
+  if (recipes.length === 0) {
+    return (
+      <div>
+        <div className={cardContainer}>
+          <h1>Hit find to search for recipes</h1>
+        </div>
+        <button type="button" onClick={searchRecipes} className={btn}>
+          Find recipes
+        </button>
+      </div>
+    );
+  }
   return (
     <div>
-      <button type="button" onClick={searchRecipes}>
-        Find recipes
-      </button>
+      {recipes.length > 1 && (
+        <button
+          type="button"
+          onClick={() => setHideRecipes(!hideRecipes)}
+          className={btn}
+        >
+          {hideRecipes ? "Show Recipes" : "Hide Recipes"}
+        </button>
+      )}
       <div className={cardContainer}>
-        {recipes.length > 1 ? (
+        {!hideRecipes && (
           recipes.map(({ recipe }) => <RecipeCard recipe={recipe} />)
-        ) : (
-          <h1>Hit find to search for recipes</h1>
         )}
       </div>
+      <button type="button" onClick={searchRecipes} className={btn}>
+        Find recipes
+      </button>
     </div>
   );
 };
