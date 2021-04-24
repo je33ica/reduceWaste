6b44a7911db57e3e4c15cdc136ad182a9ed6e87f
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-24 14:25:24 +0100

    changed api to edamam because spoonacular sucks

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,15 +1,17 @@
 const axios = require("axios");
-const apiKey = process.env.SPOONACULAR_API_KEY;
+const apiKey = process.env.EDAMAM_API_KEY;
+const appID = process.env.EDAMAM_APP_ID;
+
 
 module.exports = {
   findRecipes: (req, res) => {
-    //the spoonacular api expects a comma seperated list of ingredients
+    //the edamam api expects a comma seperated list of ingredients
     //our front end will send a post request with an array of ingredients
     //we then need to join them in to a csv string
     const ingredients = req.body.join(",");
     axios({
       method: "GET",
-      url: `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredients}&ignorePantry=true&apiKey=${apiKey}`,
+      url: `https://api.edamam.com/search?app_id=${appID}&app_key=${apiKey}&q=${ingredients}`,
     })
       .then((response) => res.json(response.data))
       .catch((err) =>
