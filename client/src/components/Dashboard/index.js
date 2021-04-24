a8ba6e725be8ee0883ed6e01e1244c9642a3dbe9
je33ica <je331687@gmail.com>
2021-04-24 17:29:20 +0100

    added colours to catagorys depenging on expiry

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,3 +1,4 @@
+import { tableContainer, headers } from "./dashboard.module.scss";
 const DashboardTable = ({ products, ingredients, updateIngredients }) => {
   const daysToExpiry = (expiry) => {
     const dateNow = Date.now();
@@ -5,50 +6,95 @@ const DashboardTable = ({ products, ingredients, updateIngredients }) => {
     return Math.floor((expiryToUnix - dateNow) / (1000 * 60 * 60 * 24));
   };
 
-  //   console.log("today", dateNow);
-  //   const expiryToUnix = Date.parse(products[0].expiry);
-  //   console.log("unix", expiryToUnix);
-  //   console.log("expiry", products[0].expiry);
-  //   console.log(
-  //     "diff",
-  //     Math.floor((expiryToUnix - dateNow) / (1000 * 60 * 60 * 24))
-  //   );
+  const categoryColour = (category, daysToExpiry) => {
+    console.log("im the cat", category);
 
-  //   const expDate = new Date(Date.now() + 863600000),
-  //   //THREEWKS = 1000 * 3600 * 24 * 7 * 3; // milliseconds in 3 weeks
+    if (category === "Dry") {
+      return dryColour(daysToExpiry);
+    } else if (category === "Frozen") {
+      return frozenColour(daysToExpiry);
+    } else if (category === "Fresh") {
+      return freshColour(daysToExpiry);
+    } else {
+      return generalColour(daysToExpiry);
+    }
+  };
+
+  const generalColour = (daysToExpiry) => {
+    if (daysToExpiry < 7) {
+      return "redAlert";
+    } else if (daysToExpiry < 14) {
+      return "orangeAlert";
+    } else {
+      return "";
+    }
+  };
+
+  const dryColour = (daysToExpiry) => {
+    if (daysToExpiry < 14) {
+      return "redAlert";
+    } else if (daysToExpiry < 28) {
+      return "orangeAlert";
+    } else {
+      return "";
+    }
+  };
+  const frozenColour = (daysToExpiry) => {
+    if (daysToExpiry < 28) {
+      return "redAlert";
+    } else if (daysToExpiry < 60) {
+      return "orangeAlert";
+    } else {
+      return "";
+    }
+  };
+  const freshColour = (daysToExpiry) => {
+    if (daysToExpiry < 3) {
+      return "redAlert";
+    } else if (daysToExpiry < 7) {
+      return "orangeAlert";
+    } else {
+      return "";
+    }
+  };
+
+  const productsComponent = products.map((product) => {
+    const daysToExpiryCondition = daysToExpiry(product.expiry);
+    const colour = categoryColour(product.category, daysToExpiryCondition);
+    return (
+      <tr className={`${colour} productTR`} key={product._id}>
+        <td>{product.productName}</td>
+        <td>{product.amount}</td>
+
+        <td>{product.category || "General"}</td>
+        <td>{product.expiry.slice(0, 10)}</td>
+        <td>{daysToExpiryCondition}</td>
+        <td>
+          <input
+            type="checkbox"
+            name={product.productName}
+            value={ingredients[product.productName]}
+            onChange={(e) => updateIngredients(e.target.name)}
+          />
+        </td>
+      </tr>
+    );
+  });
 
   return (
-    <div className="grid">
-      <div>
-        <table>
-          <tr>
-            <th>Product</th>
-            <th>Amount</th>
-            <th>Category</th>
-            <th>Expiry date</th>
-            <th>Days until expiry</th>
-            <th>Add to recipe finder</th>
-          </tr>
-          {products.map((product) => {
-            return (
-              <tr key={product._id}>
-                <td>{product.productName}</td>
-                <td>{product.amount}</td>
-                <td>{product.category || "General"}</td>
-                <td>{product.expiry.slice(0, 10)}</td>
-                <td>{daysToExpiry(product.expiry)}</td>
-                <td><input type="checkbox" name={product.productName} value={ingredients[product.productName]} onChange={(e) => updateIngredients(e.target.name)}/></td>
-              </tr>
-            );
-          })}
-          {/* <tr>
-            <td></td>
-            <td>Smith</td>
-            <td>50</td>
-          </tr> */}
-        </table>
-      </div>
-    </div>
+    <table className={tableContainer}>
+      <thead className={headers}>
+        <tr>
+          <th>Product</th>
+          <th>Amount</th>
+          <th>Category</th>
+          <th>Expiry date</th>
+          <th>Days until expiry</th>
+          <th>Add to recipe finder</th>
+        </tr>
+      </thead>
+      <tbody> {productsComponent}</tbody>
+    </table>
   );
 };
 
