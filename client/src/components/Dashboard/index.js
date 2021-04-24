b86c0c83b8e8d941d65c885c1ecd560ecf2aaac8
je33ica <je331687@gmail.com>
2021-04-24 10:42:17 +0100

    basic dashboard table started- dummy info

---
+++ client/src/components/Dashboard/index.js
@@ -0,0 +1,26 @@
+const DashboardTable = () => {
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Product</th>
+          <th>Amount</th>
+          <th>Expiry date</th>
+          <th>Days until expiry</th>
+        </tr>
+        <tr>
+          <td>Jill</td>
+          <td>Smith</td>
+          <td>50</td>
+        </tr>
+        <tr>
+          <td>Eve</td>
+          <td>Jackson</td>
+          <td>94</td>
+        </tr>
+      </table>
+    </div>
+  );
+};
+
+export default DashboardTable;
