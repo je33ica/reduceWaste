c932c9c75f986cdd0adeac0bf47fc9e8af315ebc
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-24 14:47:04 +0100

    pop up shows if no ingredients selected

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import DashboardTable from "../../components/Dashboard";
 import NavBar from "../../components/NavBar";
+import PopUpAlert from "../../components/PopUpAlert";
 import RecipeHolder from "../../components/RecipeHolder";
 import navbarIcons from "../../icons/navbarIcons";
 import API from "../../utils/api";
@@ -8,6 +9,12 @@ import API from "../../utils/api";
 const Dashboard = () => {
   const [products, setProducts] = useState([]);
   const [ingredients, setIngredients] = useState({});
+  const [recipes, setRecipes] = useState([]);
+  const [displayPopup, setDisplayPopup] = useState({
+    show: false,
+    type: "",
+    message: "",
+  });
 
   const updateIngredients = (ingredient) => {
     const tempIngredients = {...ingredients};
@@ -21,15 +28,31 @@ const Dashboard = () => {
   }
 
   const searchRecipes = () => {
+    
     const ingredientsArr = [];
     for (const key in ingredients){
       if (ingredients[key]){
         ingredientsArr.push(key)
       }
     }
+    if (ingredientsArr.length === 0){
+      setDisplayPopup({
+        show: true,
+        type: "failure",
+        message: "You need to select at least 1 ingredient",
+      })
+      setTimeout(() => {
+        setDisplayPopup({
+          show: false,
+          type: "",
+          message: "",
+        })
+      }, 2000)
+      return  
+    }
     API.findRecipes(ingredientsArr)
       .then((res) => res.json())
-      .then((results) => console.log(results))
+      .then((results) => setRecipes(results))
       .catch((err) => console.log(err));
 
   } 
@@ -53,12 +76,13 @@ const Dashboard = () => {
   return (
     <>
       <NavBar navBarItems={navBarItems} />
-      <RecipeHolder searchRecipes={searchRecipes}/>
+      <RecipeHolder searchRecipes={searchRecipes} recipes={recipes}/>
       {products.length > 0 ? (
         <DashboardTable products={products} ingredients={ingredients} updateIngredients={updateIngredients}/>
       ) : (
         <h1>No products in DB</h1>
       )}
+      {displayPopup.show && <PopUpAlert type={displayPopup.type} message={displayPopup.message}/>}
     </>
   );
 };
