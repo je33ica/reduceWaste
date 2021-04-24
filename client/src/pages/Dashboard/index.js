5cb3133aa331e3f8c5eef28c07a333a70bbcbb84
je33ica <je331687@gmail.com>
2021-04-24 12:02:05 +0100

    fetch req from DB now working

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,17 +1,32 @@
+import { useState, useEffect } from "react";
 import DashboardTable from "../../components/Dashboard";
 import NavBar from "../../components/NavBar";
+import navbarIcons from "../../icons/navbarIcons";
 
 const Dashboard = () => {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    fetch("api/users/products", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setProducts(result);
+        console.log(result);
+      });
+  }, []);
+
   const navBarItems = [
-    { path: "/account", text: "Account" },
-    { path: "/barcode", text: "Barcode scanner" },
-    { path: "/receipt", text: "Upload receipt" },
-    { path: "/addProducts", text: "Upload receipt" },
+    { path: "/account", text: "Account", icon: navbarIcons.user }, //change icon?
+    { path: "/barcode", text: "Barcode scanner", icon: navbarIcons.barcode },
+    { path: "/receipt", text: "Upload receipt", icon: navbarIcons.add }, //chnage icon
+    { path: "/addProducts", text: "Upload receipt", icon: navbarIcons.add },
   ];
+
   return (
     <>
       <NavBar navBarItems={navBarItems} />
-      <DashboardTable />;
+      <DashboardTable products={products} />;
     </>
   );
 };
