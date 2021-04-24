b1b16d0b8bc6039e599e8d9624c303b9220255d3
je33ica <je331687@gmail.com>
2021-04-24 12:01:05 +0100

    displaying products in table

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,24 +1,30 @@
-const DashboardTable = () => {
+const DashboardTable = ({ products }) => {
   return (
-    <div>
-      <table>
-        <tr>
-          <th>Product</th>
-          <th>Amount</th>
-          <th>Expiry date</th>
-          <th>Days until expiry</th>
-        </tr>
-        <tr>
-          <td>Jill</td>
-          <td>Smith</td>
-          <td>50</td>
-        </tr>
-        <tr>
-          <td>Eve</td>
-          <td>Jackson</td>
-          <td>94</td>
-        </tr>
-      </table>
+    <div className="grid">
+      <div>
+        <table>
+          <tr>
+            <th>Product</th>
+            <th>Amount</th>
+            <th>Expiry date</th>
+            <th>Days until expiry</th>
+          </tr>
+          {products.map((product) => {
+            return (
+              <tr key={product._id}>
+                <td>{product.productName}</td>
+                <td>{product.amount}</td>
+                <td>{product.expiry.slice(0, 10)}</td>
+              </tr>
+            );
+          })}
+          {/* <tr>
+            <td></td>
+            <td>Smith</td>
+            <td>50</td>
+          </tr> */}
+        </table>
+      </div>
     </div>
   );
 };
