fda7174661ac47e0c19e28f503559d4cf22c8c1e
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-24 17:30:51 +0100

    navbar stuff

--- client/src/components/NavBar/NavIcon/index.js
+++ client/src/components/NavBar/NavIcon/index.js
@@ -8,6 +8,19 @@ const NavIcon = ({icon, text, path}) => {
 
   const [displayTooltip, setTooltip] = useState(false);
 
+  if (path === "/logout"){
+    return (
+      <>
+        <div className={navIcon}>
+          <div className={holder} onMouseEnter={() => setTooltip(true)} onMouseLeave={() => setTooltip(false)}>
+          <FontAwesomeIcon icon={icon} />
+          {displayTooltip && <Tooltip text={text}/>}
+        </div>
+      </div>
+      </>
+    )
+  }
+
   return (
     <>
     <Link to={path}>
