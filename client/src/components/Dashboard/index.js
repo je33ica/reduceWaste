767e60ba3326450559e99f1294f7b7addb9542a9
je33ica <je331687@gmail.com>
2021-04-26 21:09:41 +0100

    delete route now working

--- client/src/components/Dashboard/index.js
+++ client/src/components/Dashboard/index.js
@@ -1,6 +1,11 @@
 import { tableContainer } from "./dashboard.module.scss";
 //import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
-const DashboardTable = ({ products, ingredients, updateIngredients }) => {
+const DashboardTable = ({
+  products,
+  ingredients,
+  updateIngredients,
+  removeProductFromView,
+}) => {
   const calculateDaysToExpiry = (expiry) => {
     const dateNow = Date.now();
     const expiryToUnix = Date.parse(expiry);
@@ -59,12 +64,16 @@ const DashboardTable = ({ products, ingredients, updateIngredients }) => {
 
   //sort array by date
   //mongo could sort bydate - remove anythin that has expired by 1 day
-  const productsWithColour = products.map((product) => {
+  const productsWithColour = products.reduce((acc, product) => {
     const daysToExpiryCondition = calculateDaysToExpiry(product.expiry);
+    if (daysToExpiryCondition < -3) {
+      return acc;
+    }
     const colour = categoryColour(product.category, daysToExpiryCondition);
     product.colour = colour;
-    return product;
-  });
+    acc.push(product);
+    return acc;
+  }, []);
 
   const redSorted = [];
   const orangeSorted = [];
@@ -81,20 +90,7 @@ const DashboardTable = ({ products, ingredients, updateIngredients }) => {
 
   const sortedProducts = [...redSorted, ...orangeSorted, ...generalSorted];
 
-  const removeProduct = (id) => {
-    fetch(`api/users/products${id}`, {
-      method: "DELETE",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    }).then(() => {
-      console.log("im the delte in dash", id);
-    });
-    //   // .then((response) => response.json())
-    //   .then((data) => console.log("im the data ", data)) // Manipulate the data retrieved back, if we want to do something with it
-    //   .catch((err) => console.log("im the err", err)); // Do something with the error
-    // console.log("im the id", id);
-  };
+  //UX always assuma  delete req has worked-  remove from front end first even if hasnt not from backend
 
   const productsComponent = sortedProducts.map((product) => {
     const daysToExpiryCondition = calculateDaysToExpiry(product.expiry);
@@ -112,8 +108,10 @@ const DashboardTable = ({ products, ingredients, updateIngredients }) => {
         <td>{product.category || "General"}</td>
         <td>{product.expiry.slice(0, 10)}</td>
         <td>{daysToExpiryCondition}</td>
-        <td className="operation">
-          <button onClick={() => removeProduct(product._id)}>Delete</button>
+        <td>
+          <button onClick={() => removeProductFromView(product._id)}>
+            Delete
+          </button>
         </td>
         <td>
           <input
