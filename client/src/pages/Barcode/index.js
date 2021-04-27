7a78600e2e77847a2b073b5076ff81a93df05051
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-27 22:17:02 +0100

    added product form to barcode scanner

--- client/src/pages/Barcode/index.js
+++ client/src/pages/Barcode/index.js
@@ -1,45 +1,178 @@
 import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import Loading from "../../components/Loading";
 import NavBar from "../../components/NavBar";
+import PopUpAlert from "../../components/PopUpAlert";
+import ProductForm from "../../components/ProductForm";
 import Scanner from "../../components/Scanner";
 import navbarIcons from "../../icons/navbarIcons";
 import API from "../../utils/api";
 
 const Barcode = () => {
-  
+  const date = new Date().toISOString().slice(0, 10);
   const [scanning, setScanning] = useState(false);
-  const [result, setResult] = useState(null)
+  const [result, setResult] = useState(null);
+  const [product, setProduct] = useState(null);
+  const [displayPopup, setDisplayPopup] = useState({
+    show: false,
+    type: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(null);
   const toggleScanner = () => {
-    setScanning(!scanning)
-  }
+    setScanning(!scanning);
+  };
 
   const readBarcode = (resultFromScan) => {
-    if(resultFromScan?.codeResult?.code){
+    if (resultFromScan?.codeResult?.code) {
       setResult(resultFromScan.codeResult.code);
       toggleScanner();
+      setLoading(true);
       API.searchWithBarcode(resultFromScan.codeResult.code)
-        .then(res => {
-          console.log(res)
+        .then((res) => {
+          setLoading(false);
+          if (res.status === 200) {
+            return res.json();
+          }
+          setProduct({
+            productName: "",
+            amount: "",
+            expiry: date,
+            id: uuid(),
+            EAN: "",
+            category: "",
+          });
         })
+        .then((parsed) => {
+          setProduct(parsed);
+        })
+        .catch((err) => {
+          setLoading(false);
+          setDisplayPopup({
+            show: true,
+            type: "failure",
+            message:
+              "Sorry, our server is currently not responding. Please try again later",
+          });
+          setTimeout(() => {
+            setDisplayPopup({
+              show: false,
+              type: "",
+              message: "",
+            });
+          }, 1500);
+        });
     }
-    return
-  }
+    return;
+  };
 
+  const submitProductHandler = (e) => {
+    e.preventDefault();
+
+    //before fetch -> render the loading component
+    //insde 2nd .then -> unrender loading component
+    //render a a pop up alert based on the response
+    //hanlde the promise relut with the succes/fail message - use registration form
+    //the server expects an array
+    setLoading(true);
+    //submit the registration form
+    fetch("api/users/products", {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify([product]),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        setLoading(false);
+        if (result.message) {
+          setDisplayPopup({
+            show: true,
+            type: "failure",
+            message: result.message,
+          });
+        } else {
+          setDisplayPopup({
+            show: true,
+            type: "success",
+            message: "Products successfully saved",
+          });
+          setTimeout(() => {
+            setDisplayPopup({
+              show: false,
+              type: "",
+              message: "",
+            });
+          }, 1500);
+        }
+      })
+      .catch((err) => {
+        setDisplayPopup({
+          show: true,
+          type: "failure",
+          message:
+            "Sorry but your product could not be saved right now, please try again later",
+        });
+        setTimeout(() => {
+          setDisplayPopup({
+            show: false,
+            type: "",
+            message: "",
+          });
+        }, 1500);
+      });
+  };
+
+  const updateElement = (value, target, id) => {
+    let elementUpdated = false;
+    const tempResults = [product];
+    for (let i = 0; i < tempResults.length && !elementUpdated; i++) {
+      if (tempResults[i].id === id) {
+        tempResults[i][target] = value;
+        elementUpdated = true;
+      }
+    }
+    setProduct(tempResults[0]);
+  };
 
   const navBarItems = [
-    { path: "/account", text: "Account", icon: navbarIcons.user }, 
-    { path: "/dashboard", text: "Your store cupboard", icon: navbarIcons.barcode },
+    { path: "/account", text: "Account", icon: navbarIcons.user },
+    {
+      path: "/dashboard",
+      text: "Your store cupboard",
+      icon: navbarIcons.barcode,
+    },
     { path: "/receipt", text: "Upload receipt", icon: navbarIcons.upload },
     { path: "/addProducts", text: "Add products", icon: navbarIcons.add },
   ];
   return (
     <>
       <NavBar navBarItems={navBarItems} />
-      <h1>Barcode page</h1>
-      <button type="button" onClick={toggleScanner}>{scanning ? "Stop" : "Start Scanner"}</button>
-      {scanning && <Scanner readBarcode={readBarcode} />}
-      {result && <h1>Your barcode is {result}</h1>}
+      <div style={{ textAlign: "center" }}>
+        <h1>Barcode page</h1>
+        <button className="btn" type="button" onClick={toggleScanner}>
+          {scanning ? "Stop" : "Start Scanner"}
+        </button>
+        {scanning && <Scanner readBarcode={readBarcode} />}
+        {result && <h4>Your barcode is {result}</h4>}
+        {loading && <Loading />}
+        {product && (
+          <ProductForm
+            updateElement={updateElement}
+            productsArr={[product]}
+            submitProductCardstoDB={submitProductHandler}
+            loading={loading}
+            displayPopup={displayPopup}
+          />
+        )}
+        {displayPopup.show && (
+          //popup
+          <PopUpAlert type={displayPopup.type} message={displayPopup.message} />
+        )}
+      </div>
     </>
-  )
+  );
 };
 
 export default Barcode;
