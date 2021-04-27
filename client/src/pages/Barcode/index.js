bf53f3a4d94b2b7a5870eaa0afcdb7770e2ff785
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-27 20:33:34 +0100

    implement scanner logic - positioned elements correctly, barcode is logged to screen

--- client/src/pages/Barcode/index.js
+++ client/src/pages/Barcode/index.js
@@ -1,5 +1,40 @@
-const Barcode = () => {
-  return <h1> Welcome to the Barcode page</h1>;
+import { useState } from "react";
+import NavBar from "../../components/NavBar";
+import Scanner from "../../components/Scanner";
+import navbarIcons from "../../icons/navbarIcons";
+
+const Barcode = (props) => {
+  
+  const [scanning, setScanning] = useState(false);
+  const [result, setResult] = useState(null)
+  const toggleScanner = () => {
+    setScanning(!scanning)
+  }
+
+  const readBarcode = (result) => {
+    if(result?.codeResult?.code){
+      setResult(result.codeResult.code);
+      toggleScanner()
+    }
+    return
+  }
+
+
+  const navBarItems = [
+    { path: "/account", text: "Account", icon: navbarIcons.user }, //change icon?
+    { path: "/dashboard", text: "Your store cupboard", icon: navbarIcons.barcode },
+    { path: "/receipt", text: "Upload receipt", icon: navbarIcons.upload }, //chnage icon
+    { path: "/addProducts", text: "Add products", icon: navbarIcons.add },
+  ];
+  return (
+    <>
+      <NavBar navBarItems={navBarItems} />
+      <h1>Barcode page</h1>
+      <button type="button" onClick={toggleScanner}>{scanning ? "Stop" : "Start Scanner"}</button>
+      {scanning && <Scanner readBarcode={readBarcode} />}
+      {result && <h1>Your barcode is {result}</h1>}
+    </>
+  )
 };
 
 export default Barcode;
