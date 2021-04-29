57916a5aaea7771bd435cae4cce676ccb1319cf4
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-29 20:06:53 +0100

    styling on response div

--- client/src/pages/RequestReset/index.js
+++ client/src/pages/RequestReset/index.js
@@ -5,24 +5,25 @@ import navbarIcons from "../../icons/navbarIcons";
 import API from "../../utils/api";
 
 const RequestReset = () => {
-
   const [outputResponse, setOutputResponse] = useState(null);
 
   const emailInput = useRef("");
 
   const requestResetHandler = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const enteredEmail = emailInput.current.value;
-    API.requestPasswordReset(enteredEmail).then(res => {
-      console.log(res)
-      if (res.status === 400){
+    API.requestPasswordReset(enteredEmail).then((res) => {
+      console.log(res);
+      if (res.status === 400) {
         //set state to display error message
         setOutputResponse("That email address wasn't recognised");
       } else {
-        setOutputResponse("An email containing a unique link to reset your password should be in your inbox (it may be in your junk)");
+        setOutputResponse(
+          "An email containing a unique link to reset your password should be in your inbox (it may be in your junk)"
+        );
       }
-    })
-  }
+    });
+  };
 
   const navBarItems = [
     { path: "/login", text: "Login", icon: navbarIcons.login },
@@ -31,10 +32,22 @@ const RequestReset = () => {
   return (
     <>
       <NavBar navBarItems={navBarItems} />
-      <Reset emailInput={emailInput} requestResetHandler={requestResetHandler}/>
-      {outputResponse && <div style={{textAlign: "center"}}>
-        <p>{outputResponse}</p>
-        </div>}
+      <Reset
+        emailInput={emailInput}
+        requestResetHandler={requestResetHandler}
+      />
+      {outputResponse && (
+        <div
+          style={{
+            textAlign: "center",
+            width: "90%",
+            maxWidth: "600px",
+            margin: "0 auto",
+          }}
+        >
+          <p>{outputResponse}</p>
+        </div>
+      )}
     </>
   );
 };
