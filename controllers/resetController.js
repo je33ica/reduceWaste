0d4c8bd53fdc055a61c827707307880a4feac675
Ross McIntyre <rossmci90@hotmail.co.uk>
2021-04-29 18:50:07 +0100

    created try catch block for email request

--- controllers/resetController.js
+++ controllers/resetController.js
@@ -2,7 +2,13 @@ const resetService = require("../utils/resetAuth");
 
 module.exports = {
   sendResetEmail: async (req, res) => {
-    const requestPasswordResetResult = await resetService.requestPasswordReset(req.body.email);
-    return res.json(requestPasswordResetResult)
+    try {
+      const requestPasswordResetResult = await resetService.requestPasswordReset(req.body.email);
+      return res.json(requestPasswordResetResult)
+    } catch (error) {
+      res.status(400)
+      res.send(error)
+    }
+
   } 
 }
